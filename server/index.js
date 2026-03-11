c3bc3694b6c935d2d222ad89163820ae86951005
Himanshu Joshi <150330961+himanshuJoshi9520@users.noreply.github.com>
2026-03-11 21:39:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import questionRouter from "./routes/question.route.js"
 
 const app = express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://jobprep-ejz8.onrender.com",
     credentials:true
 }))
 
